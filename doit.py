ef550d78d45c347e7c35e88928f2d89f5c821c17
Tim Gee <twgee@ncsu.edu>
2021-01-28 16:24:44 -0500

    corrected finename handeling

--- doit.py
+++ doit.py
@@ -6,6 +6,7 @@ Created on Jan 27, 2021
 import requests
 from bs4 import BeautifulSoup as BS
 import pdfkit
+import time
 
 
 
@@ -14,9 +15,9 @@ def doit(directory,list_to_process):
     numnext=1
     try:
         for url in list_to_process:
+            url=url.strip()
             print("-----------------\nNow processing %s" % url)
             print("Number %d of %d" % (numnext,numtodo))
-            numnext+=1
             code = requests.get(url)
             plain = code.text
             s=BS(plain,"html.parser")
@@ -28,9 +29,12 @@ def doit(directory,list_to_process):
                 l.extract()
             todo="<meta charset='utf-8'ex/>"+str(article)+str(footer)
             #build the filename
+            
             name=url.split('/')[-2]
-            outfile=directory+"/"+name+".pdf"
+            outfile=directory+"/"+name+"_"+str(numnext)+".pdf"
             pdfkit.from_string(todo,outfile)
+            numnext+=1
+            #time.sleep(3)
     except Exception:
         print("Error processing %s" % url)
         fn=open("failed_save.txt","w")
