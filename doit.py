5b48ec3b9226e6a82d4ddcee3c647e8b8a151ea2
Tim Gee <twgee@ncsu.edu>
2021-01-28 10:21:54 -0500

    First Files

---
+++ doit.py
@@ -0,0 +1,44 @@
+'''
+Created on Jan 27, 2021
+
+@author: timmygee
+'''
+import requests
+from bs4 import BeautifulSoup as BS
+import pdfkit
+
+
+
+def doit(directory,list_to_process):
+    numtodo=len(list_to_process)
+    numnext=1
+    try:
+        for url in list_to_process:
+            print("-----------------\nNow processing %s" % url)
+            print("Number %d of %d" % (numnext,numtodo))
+            numnext+=1
+            code = requests.get(url)
+            plain = code.text
+            s=BS(plain,"html.parser")
+            article=s.article
+            footer=s.footer
+# now strip all the links
+            links=footer.findAll('a')
+            for l in links:
+                l.extract()
+            todo="<meta charset='utf-8'ex/>"+str(article)+str(footer)
+            #build the filename
+            name=url.split('/')[-2]
+            outfile=directory+"/"+name+".pdf"
+            pdfkit.from_string(todo,outfile)
+    except Exception:
+        print("Error processing %s" % url)
+        fn=open("failed_save.txt","w")
+        fn.write(todo)
+        fn.close()
+        return 2
+            
+
+        
+    
+
